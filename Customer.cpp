fa94f9d7cbb8df70602d7591c9dcfa2b7e31256e
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2023-06-10 12:50:41 +0400

    commit of the Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,23 @@
+// Customer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+class Person {
+    char firstname[25];
+    char surname[25];
+    char patronymic[25];
+
+    char birthday[15];
+    char address[50];
+    char phone[15];
+
+    char passportID[10];
+    char socialcard[10];
+};
+
+int main()
+{
+    Person person;
+}
+
