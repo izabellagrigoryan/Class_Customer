8a67a203ae79d7a9e561874d9784f7fbdbd58bd6
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2023-06-10 21:13:03 +0400

    third commit of the Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -3,6 +3,14 @@
 
 #include <iostream>
 #include <string>
+
+class Account {
+
+    std::string account_number;
+    std::string account_type;
+    std::string account_amount;
+};
+
 class Person {
     std::string firstname;
     std::string surname;
@@ -14,11 +22,26 @@ class Person {
 
     std::string passportID;
     std::string socialcard;
+    std::string xerox_filename;
+
+    Account accounts[5];
+
+public:
+    Person(){}
+    Person(std::string passportID, std::string socialcard)
+    {
+        this->passportID = passportID;
+        this->socialcard = socialcard;
+    }
+
+    void init_person(){}
+    void open_account(){}
+    void close_account(){}
 };
 
 int main()
 {
-    Person person;
+    Person person("AB123456", "1234567");
     return 0;
 }
 
